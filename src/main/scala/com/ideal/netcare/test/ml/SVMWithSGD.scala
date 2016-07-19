ff3cdb5807b5587f75e927b1e061ea836a92017d
jonathonfly <sunyunfei12040@163.com>
2016-07-19 10:49:11 +0800

    SVMWithSGDTest

---
+++ src/main/scala/com/ideal/netcare/test/ml/SVMWithSGD.scala
@@ -0,0 +1,50 @@
+package com.ideal.netcare.test.ml
+
+import org.apache.spark.mllib.classification.SVMWithSGD
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+ * Created by syf on 2016/7/19.
+ */
+object SVMWithSGDTest {
+  def main(args: Array[String]) {
+    val conf = new SparkConf()
+    conf.setAppName("Spark Dijkstra").setMaster("spark://spark-master:7077").set("spark.executor.memory", "512m")
+    val sc = new SparkContext(conf)
+    //本地打包的jar的位置  必备
+    sc.addJar("target/scala-2.10/spark-test_2.10-1.0.jar")
+
+    //加载、解析训练、测试数据文件
+    //训练数据
+    val trainData = sc.textFile("hdfs://spark-master:9000/syf/spark/data/ml/svm/sample_svm_train_data.txt")
+    val parsedTrainData=trainData.map { line =>
+      val parts = line.split("\\s+")
+      LabeledPoint(parts(0).toDouble, Vectors.dense(parts.tail.map(x => x.toDouble)))
+    }
+    //测试数据
+    val testData = sc.textFile("hdfs://spark-master:9000/syf/spark/data/ml/svm/sample_svm_test_data.txt")
+    val parsedTestData=testData.map { line =>
+      val parts = line.split("\\s+")
+      LabeledPoint(parts(0).toDouble, Vectors.dense(parts.tail.map(x => x.toDouble)))
+    }
+
+    //设置迭代次数并训练模型
+    //10次迭代 ，分类错误率 trainErr = 0.40625
+    //20次迭代 ，分类错误率 trainErr = 0.390625
+    //30次迭代 ，分类错误率 trainErr = 0.390625 算法已经收敛
+    val numIterations = 20
+    val model=SVMWithSGD.train(parsedTrainData,numIterations)
+
+    //模型预测测试数据结果
+    val labelAndPreds=parsedTestData.map{point=>
+      val prediction=model.predict(point.features)
+      (point.label,prediction)
+    }
+
+    //计算分类错误率
+    val trainErr=labelAndPreds.filter(r=>r._1!=r._2).count.toDouble/parsedTestData.count
+    println(s"trainErr = ${trainErr}")
+  }
+}
--- src/main/scala/com/ideal/netcare/test/ml/ToolsTest.scala
+++ src/main/scala/com/ideal/netcare/test/ml/ToolsTest.scala
@@ -2,7 +2,6 @@ package com.ideal.netcare.test.ml
 
 import org.apache.spark.mllib.linalg.{Matrices, Matrix, Vector, Vectors}
 import org.apache.spark.mllib.regression.LabeledPoint
-import org.apache.spark.rdd.RDD
 
 /**
  * Created by syf on 2016/7/18.
@@ -11,6 +10,7 @@ object ToolsTest {
   def main(args: Array[String]) {
     //向量
     //密集向量
+    println("向量")
     val dv: Vector = Vectors.dense(1.0, 0.0, 3.0)
     //稀疏向量
     val sv: Vector = Vectors.sparse(3, Array(0, 2), Array(1.0, 3.0))
@@ -18,16 +18,54 @@ object ToolsTest {
     println(s"dv=${dv}")
     println(s"sv=${sv}")
     println(s"sv2=${sv2}")
+    println
 
     //标量
+    println("标量")
     val pos = LabeledPoint(1.0, Vectors.dense(1.0, 0.0, 3.0))
     val neg = LabeledPoint(0.0, Vectors.sparse(3, Array(0, 2), Array(1.0, 3.0)))
     println(s"pos=${pos}")
     println(s"neg=${neg}")
+    println
 
     //矩阵
+    println("矩阵")
     val dm:Matrix=Matrices.dense(3,2,Array(1.0,3.0,5.0,2.0,4.0,6.0))
     println("dm=")
     println(dm)
+    println
+
+    //符号->  A->B表示(A,B)   功能：转化为二元组
+    println("符号->")
+    val a="abc"
+    val b=List(1,2,3)
+    val d=Array(4,5)
+    val c=a->b->d
+    println(s"c._1._1=${c._1._1}")
+    c._1._2.foreach(println)
+    c._2.toVector.foreach(println)
+    println
+
+    //符号<-  用于遍历集合中的元素
+    println("符号<-")
+    val ver=c._2.toVector
+    for(v<-ver){
+      println(v)
+    }
+    println
+
+    println("Option")
+    val myMap:Map[String,String]=Map("key1"->"value1")
+    val value1:Option[String]=myMap.get("key1")
+    val value2:Option[String]=myMap.get("key2")
+
+    value1 match{
+      case Some(value) => println(value.length)
+      case None => println("None")
+    }
+
+    val value3=value2.getOrElse(0)
+    println(value3)
+    println
   }
 }
